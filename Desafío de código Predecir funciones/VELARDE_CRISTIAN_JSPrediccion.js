c66fcbeb1ff5c3868c1f71e86caf60f7f0577d34
CristianVelarde98 <cristianvelardefx@gmail.com>
2023-06-22 16:44:24 -0300

    Desafío de código: Predecir funciones

---
+++ Desafío de código Predecir funciones/VELARDE_CRISTIAN_JSPrediccion.js
@@ -0,0 +1,20 @@
+// Prediccion 1
+function myBirthYearFunc() {
+  console.log("Nací en " + 1980); // "Naci en 1980"
+}
+
+// Prediccion 2
+function myBirthYearFunc(EntradaAñoNacimiento) {
+  console.log("Nací en " + EntradaAñoNacimiento); // "Naci en 1980"
+}
+// myBirthYearFunc(1980);
+
+// Prediccion 3
+function add(num1, num2) {
+  console.log("¡Sumando números!"); // "¡Sumando números!"
+  console.log("num1 is: " + num1); // "num1 is: 10"
+  console.log("num2 is: " + num2); // "num2 is: 20"
+  var sum = num1 + num2;
+  console.log(sum); // 30
+}
+// add(10, 20);
